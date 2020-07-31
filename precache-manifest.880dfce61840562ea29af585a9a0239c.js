3be59fe8500cbdd555a787d4a988f75cbad8347c
TheLevius <reflevius@gmail.com>
2020-07-31 03:39:22 +0300

    Updates

---
+++ precache-manifest.880dfce61840562ea29af585a9a0239c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183fc7444348347b03b10b5c659dd905",
+    "url": "/incubator-cards/index.html"
+  },
+  {
+    "revision": "6a9faee504ad8e49bde6",
+    "url": "/incubator-cards/static/css/main.8a887e6e.chunk.css"
+  },
+  {
+    "revision": "925382e37e962d11ed1e",
+    "url": "/incubator-cards/static/js/2.2c6f6fa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/incubator-cards/static/js/2.2c6f6fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a9faee504ad8e49bde6",
+    "url": "/incubator-cards/static/js/main.ca5f033c.chunk.js"
+  },
+  {
+    "revision": "ed48db6099428c46c9ea",
+    "url": "/incubator-cards/static/js/runtime-main.1d33fdeb.js"
+  }
+]);
